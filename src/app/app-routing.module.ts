9fd40007fb6363f683fad6a3a0d9abc54e468047
Hansel Cabrera <hcabrera@segurnet.com.do>
2023-05-19 10:53:40 -0400

    Agregando disc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PersonaModule } from './persona/persona.module';
+import { IndexComponent } from './persona/index/index.component';
 
 
 
 const routes: Routes = [
-  { path: '', component: PersonaModule },
+  { path: '', component: IndexComponent },
 ];
 
 @NgModule({
